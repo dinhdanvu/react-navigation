21a864c92055d932f0321ffb229aeb4c387cf0ee
DinhNguyen <dinhdanvu@gmail.com>
2020-08-22 07:16:39 +0700

    2.18.3

--- packages/devtools/src/index.tsx
+++
@@ -1,9 +0,0 @@
-const noop: any = () => {};
-
-export let useReduxDevToolsExtension: typeof import('./useReduxDevToolsExtension').default;
-
-if (process.env.NODE_ENV !== 'production') {
-  useReduxDevToolsExtension = require('./useReduxDevToolsExtension').default;
-} else {
-  useReduxDevToolsExtension = noop;
-}
